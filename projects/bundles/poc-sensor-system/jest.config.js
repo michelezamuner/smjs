7733668fe25c5398b67a47e66bca6242e49c79e7
Michele Zamuner <michelezamuner@gmail.com>
2019-03-17 10:11:28 +0100

    Introduced sensor system POC

---
+++ projects/bundles/poc-sensor-system/jest.config.js
@@ -0,0 +1,24 @@
+const config = {
+    roots: ['./tests'],
+    testEnvironment: 'node',
+    verbose: true,
+    coverageDirectory: '/tmp',
+    coveragePathIgnorePatterns: [],
+    coverageReporters: ['text'],
+};
+
+if (process.env.JEST_ENV === 'ci') {
+    config.bail = true;
+    config.verbose = false;
+    config.coverageReporters = [];
+    config.coverageThreshold = {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        }
+    };
+}
+
+module.exports = config;
